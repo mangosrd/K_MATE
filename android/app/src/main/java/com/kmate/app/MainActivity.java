4d4841b8f95c817e3702db95660c7eea7e343101
mangosrd <k2829362@gmail.com>
2026-08-04 17:31:16 +0900

    fix: register native Google plugin before bridge

--- android/app/src/main/java/com/kmate/app/MainActivity.java
+++ android/app/src/main/java/com/kmate/app/MainActivity.java
@@ -5,7 +5,7 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(android.os.Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         registerPlugin(NativeGoogleAuthPlugin.class);
+        super.onCreate(savedInstanceState);
     }
 }
